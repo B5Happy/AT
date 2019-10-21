51404ca20b9957dff300265cc46f37ef4400709c
Saly <saly@juke4me.com>
2019-10-21 22:26:10 +0200

    first commit

---
+++ Bigil/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Bigil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AppConfig desConfig = JsonConvert.DeserializeObject<AppConfig>(File.ReadAllText("../../../secrets.json"));
+            string accountSid = desConfig.AccountSid ;
+            string authToken = desConfig.AuthToken;
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+                from: new Twilio.Types.PhoneNumber("+12192455815"),
+                to: new Twilio.Types.PhoneNumber("+33773275750")
+            );
+
+            Console.WriteLine(message.Sid);
+
+        }
+    }
+}
---
+++ Bigil/appconfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bigil
+{
+    public class AppConfig
+    {
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+    }
+}
